fb54ca5f671217f1f49fb8251fb7f5869c541521
Flinta Ovidiu <ovidiu.flinta@gmail.com>
2024-05-14 16:59:11 +0300

    Add new interfaces2

--- app/src/main/java/com/example/mypublictransportlogin/ClientMain.kt
+++ app/src/main/java/com/example/mypublictransportlogin/ClientMain.kt
@@ -1,29 +1,27 @@
 package com.example.mypublictransportlogin
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RelativeLayout
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 
 class ClientMain : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.client_main)
-        val relativeLayout = findViewById<RelativeLayout>(R.id.main)
-        val animationDrawable = relativeLayout.background as AnimationDrawable
-        animationDrawable.setEnterFadeDuration(3500)
-        animationDrawable.setExitFadeDuration(5000)
-        animationDrawable.start()
 
-        findViewById<ImageButton>(R.id.Route).setOnClickListener {
+        findViewById<androidx.cardview.widget.CardView>(R.id.Route).setOnClickListener {
             val intent1 = Intent(this, BussLines::class.java)
             startActivity(intent1)
         }
 
-        findViewById<Button>(R.id.BuyPassButton).setOnClickListener {
+        findViewById<Button>(R.id.BuyPassCard).setOnClickListener {
             val intent11 = Intent(this, SelectBox::class.java)
             startActivity(intent11)
         }
